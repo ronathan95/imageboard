c45d24aded24dc4bf651064fe7a965ec07e7fa0b
Roni <roninathan95@gmail.com>
2020-12-14 19:11:27 +0100

    removed comments from code

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,3 @@
-// new Vue({
-//     el: "#main",
-//     data: {
-//         name: "Jasmine",
-//         seen: false,
-//         cities: [],
-//         myClassName: "peter",
-//     },
-//     // mounted is a lifecycle method that can be accessed
-//     mounted: function () {
-//         // console.log("my Vue component has mounted!");
-//         console.log("this before axios: ", this);
-//         var self = this;
-//         axios
-//             .get("/cities")
-//             .then(function (response) {
-//                 console.log("self: ", self);
-//                 console.log("response.data: ", response.data);
-//                 console.log("this inside then: ", this);
-//                 self.cities = response.data;
-//             })
-//             .catch(function (err) {
-//                 console.error("error: ", err);
-//             });
-//     },
-//     methods: {
-//         petesMethod: function (city) {
-//             console.log("Pete's method :)", city);
-//             // change the value of the name on data
-//             this.name = city;
-//         },
-//     },
-// });
-
 new Vue({
     el: "#gallery",
     data: {
